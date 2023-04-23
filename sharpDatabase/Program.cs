c63b562b31629a1ee191252c13f4db2f9f53540b
Ugyen Lama <ugyenchheda@gmail.com>
2023-04-23 21:17:11 +0300

    Delete information from database

--- sharpDatabase/Program.cs
+++ sharpDatabase/Program.cs
@@ -94,51 +94,88 @@ namespace OOPTask13
 
                     //Modify information
 
-                    string modify = "UPDATE Dentist SET TelNum=@newTelNum WHERE Name=@dName";
+                //    string modify = "UPDATE Dentist SET TelNum=@newTelNum WHERE Name=@dName";
 
-                    SqlCommand command = new SqlCommand(modify, sqlConnection);
+                //    SqlCommand command = new SqlCommand(modify, sqlConnection);
 
-                    SqlParameter sqlParameter = new SqlParameter
+                //    SqlParameter sqlParameter = new SqlParameter
+                //    {
+                //        ParameterName = "@dName",
+                //        Value = "Jukka Ikonen",
+                //        SqlDbType = System.Data.SqlDbType.NVarChar
+                //    };
+
+                //    command.Parameters.Add(sqlParameter);
+
+                //    sqlParameter = new SqlParameter
+                //    {
+                //        ParameterName = "@newTelNum",
+                //        Value = "050 12344444",
+                //        SqlDbType = System.Data.SqlDbType.NVarChar
+                //    };
+                //    command.Parameters.Add(sqlParameter);
+
+                //    int numberOfRows = command.ExecuteNonQuery();
+                //    if (numberOfRows > 0)
+                //        Console.WriteLine("Successfully updated information.");
+                //    else if (numberOfRows == 0)
+                //        Console.WriteLine("No such employee in the company.");
+
+                //    //Did anything go to the database table?
+
+                //    Console.WriteLine("After possibly updating information:");
+                //    string queryString = "SELECT * FROM Dentist";
+                //    command = new SqlCommand(queryString, sqlConnection);
+                //    using (SqlDataReader reader = command.ExecuteReader())
+                //    {
+                //        while (reader.Read())
+                //        {
+                //            Console.WriteLine(String.Format("{0}, {1}, {2}",
+                //                reader[0], reader[1], reader[2]));
+                //        }
+                //    }
+                //}
+
+
+                //Delete from Database
+
+
+                                    string remove = "DELETE FROM Dentist WHERE Name=@dName";
+
+                SqlCommand command = new SqlCommand(remove, sqlConnection);
+
+                SqlParameter sqlParameter = new SqlParameter
+                {
+                    ParameterName = "@dName",
+                    Value = "Paula Oksanen",
+                    SqlDbType = System.Data.SqlDbType.NVarChar
+                };
+                command.Parameters.Add(sqlParameter);
+
+
+
+                int numberOfRows = command.ExecuteNonQuery();
+                //NOTICE: Go back to previous cases, and make a change!
+                if (numberOfRows > 0)
+                    Console.WriteLine("Successfully removed information.");
+                else if (numberOfRows == 0)
+                    Console.WriteLine("No such employee in the company.");
+
+                //Did anything go to the database table?
+
+                Console.WriteLine("After possibly removing information:");
+                string queryString = "SELECT * FROM Dentist";
+                command = new SqlCommand(queryString, sqlConnection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
                     {
-                        ParameterName = "@dName",
-                        Value = "Jukka Ikonen",
-                        SqlDbType = System.Data.SqlDbType.NVarChar
-                    };
-
-
-
-
-
-                    command.Parameters.Add(sqlParameter);
-
-                    sqlParameter = new SqlParameter
-                    {
-                        ParameterName = "@newTelNum",
-                        Value = "050 12344444",
-                        SqlDbType = System.Data.SqlDbType.NVarChar
-                    };
-                    command.Parameters.Add(sqlParameter);
-
-                    int numberOfRows = command.ExecuteNonQuery();
-                    if (numberOfRows > 0)
-                        Console.WriteLine("Successfully updated information.");
-                    else if (numberOfRows == 0)
-                        Console.WriteLine("No such employee in the company.");
-
-                    //Did anything go to the database table?
-
-                    Console.WriteLine("After possibly updating information:");
-                    string queryString = "SELECT * FROM Dentist";
-                    command = new SqlCommand(queryString, sqlConnection);
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            Console.WriteLine(String.Format("{0}, {1}, {2}",
-                                reader[0], reader[1], reader[2]));
-                        }
+                        Console.WriteLine(String.Format("{0}, {1}, {2}",
+                            reader[0], reader[1], reader[2]));
                     }
                 }
+            }
+
 
                 catch (Exception ex)
                 {
