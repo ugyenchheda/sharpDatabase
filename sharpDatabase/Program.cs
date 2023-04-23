0f81ebe7105b35245b722f09ee67c81141f84dbe
Ugyen Lama <ugyenchheda@gmail.com>
2023-04-23 21:11:13 +0300

    search in database

--- sharpDatabase/Program.cs
+++ sharpDatabase/Program.cs
@@ -11,13 +11,51 @@ namespace OOPTask13
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
                 try
-                {
+              {
                     sqlConnection.Open();
-                    string insertNew = "INSERT INTO Dentist (Name, TelNum) VALUES (@newName, @newTelNum)";
-                    SqlCommand command = new SqlCommand(insertNew, sqlConnection);
+            //        string insertNew = "INSERT INTO Dentist (Name, TelNum) VALUES (@newName, @newTelNum)";
+            //        SqlCommand command = new SqlCommand(insertNew, sqlConnection);
+            //        SqlParameter sqlParameter = new SqlParameter
+            //        {
+            //            ParameterName = "@newName",
+            //            Value = "Reijo Vuohelainen",
+            //            SqlDbType = System.Data.SqlDbType.NVarChar
+            //        };
+            //        command.Parameters.Add(sqlParameter);
+
+            //        sqlParameter = new SqlParameter
+            //        {
+            //            ParameterName = "@newTelNum",
+            //            Value = "040 1119991",
+            //            SqlDbType = System.Data.SqlDbType.NVarChar
+            //        };
+            //        command.Parameters.Add(sqlParameter);
+            //        int numberOfRows = command.ExecuteNonQuery();
+            //        if (numberOfRows > 0)
+            //            Console.WriteLine("Successfully inserted information.");
+            //        else if (numberOfRows == 0)
+            //            Console.WriteLine("No such employee in the company.");
+
+            //        //Did anything go to the database table?
+
+            //        Console.WriteLine("After possibly inserting a row:");
+            //        string queryString = "SELECT * FROM Dentist";
+            //        command = new SqlCommand(queryString, sqlConnection);
+            //        using (SqlDataReader reader = command.ExecuteReader())
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                Console.WriteLine(String.Format("{0}, {1}, {2}",
+            //                    reader[0], reader[1], reader[2]));
+            //            }
+            //        }
+            //    }
+                    string findOne = "SELECT * FROM Dentist WHERE Name=@personInInterest";
+                    SqlCommand command = new SqlCommand(findOne, sqlConnection);
+
                     SqlParameter sqlParameter = new SqlParameter
                     {
-                        ParameterName = "@newName",
+                        ParameterName = "@personInInterest",
                         Value = "Reijo Vuohelainen",
                         SqlDbType = System.Data.SqlDbType.NVarChar
                     };
@@ -27,38 +65,29 @@ namespace OOPTask13
 
 
 
-                    sqlParameter = new SqlParameter
-                    {
-                        ParameterName = "@newTelNum",
-                        Value = "040 1119991",
-                        SqlDbType = System.Data.SqlDbType.NVarChar
-                    };
-                    command.Parameters.Add(sqlParameter);
-                    int numberOfRows = command.ExecuteNonQuery();
-                    if (numberOfRows > 0)
-                        Console.WriteLine("Successfully inserted information.");
-                    else if (numberOfRows == 0)
-                        Console.WriteLine("No such employee in the company.");
+                    //Do we have the person????                 
 
-                    //Did anything go to the database table?
-
-                    Console.WriteLine("After possibly inserting a row:");
-                    string queryString = "SELECT * FROM Dentist";
-                    command = new SqlCommand(queryString, sqlConnection);
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        while (reader.Read())
+                        if (reader.HasRows)
                         {
-                            Console.WriteLine(String.Format("{0}, {1}, {2}",
-                                reader[0], reader[1], reader[2]));
+                            while (reader.Read())
+                            {
+                                Console.WriteLine(String.Format("{0}, {1}, {2}",
+                                    reader[0], reader[1], reader[2]));
+                            }
                         }
+                        else
+                            Console.WriteLine("No such employee here.");
                     }
                 }
+
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
+
         }
     }
 }
